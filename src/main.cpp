43e0acb0f0a7692d1de868871f3ab790159238ad
sd1827 <sharvaridhile@gmail.com>
2026-02-20 14:32:12 -0500

    Code for finding values

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,113 @@
-#include <FEH.h>
-#include <Arduino.h>
 #include <FEHLCD.h>
-#include <FEHMotor.h>
-#include <FEHServo.h>
+#include <FEHIO.h>
+#include <FEHSD.h>
 #include <FEHUtility.h>
 
-
-
-// Declare things like Motors, Servos, etc. here
-// For example:
-// FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
-// FEHServo servo(FEHServo::Servo0);
-
+// Declarations for analog optosensors
+AnalogInputPin right_opto(FEHIO::Pin8);
+AnalogInputPin center_opto(FEHIO::Pin10);
+AnalogInputPin left_opto(FEHIO::Pin12);
 
 void ERCMain()
 {
-    DigitalInputPin one(FEHIO::Pin11);
-    DigitalInputPin two(FEHIO::Pin2);
-
-    FEHMotor right_motor(FEHMotor::Motor3, 9.0);
-    FEHMotor left_motor(FEHMotor::Motor2, 9.0);
-
-
-    right_motor.SetPercent(20);
-    left_motor.SetPercent(-20);
     
+    // int x, y; 
 
-    while (true) {
-        while (one.Value() != 0) {};
-        right_motor.Stop();
-        left_motor.Stop();
-        Sleep(1.0);
-        right_motor.SetPercent(-20);
-        left_motor.SetPercent(5);
-        Sleep(3.0);
-        right_motor.Stop();
-        left_motor.Stop();
-        right_motor.SetPercent(20);
-        left_motor.SetPercent(-20);
+    // //Initialize the screen
+    // LCD.Clear(BLACK);
+    // LCD.SetFontColor(WHITE);
 
-        while (one.Value() != 0) {};
-        right_motor.Stop();
-        left_motor.Stop();
-        Sleep(1.0);
-        right_motor.SetPercent(5);
-        left_motor.SetPercent(20);
-        Sleep(3.0);
-        right_motor.Stop();
-        left_motor.Stop();
-        right_motor.SetPercent(20);
-        left_motor.SetPercent(-20);
+    // LCD.WriteLine("Analog Optosensor Testing");
+    // LCD.WriteLine("Touch the screen");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
 
-        // while (one.Value() != 0) {};
-        // right_motor.Stop();
-        // left_motor.Stop();
-        // Sleep(1.0);
-        // right_motor.SetPercent(20);
-        // left_motor.SetPercent(-5);
-        // Sleep(4.0);
-        // right_motor.SetPercent(20);
-        // left_motor.SetPercent(20);
-    
-    }
+    // // Record values for optosensors on and off of the straight line
+    // // Left Optosensor on straight line
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Place left optosensor on straight line");
+    // Sleep(1.0); // Wait to avoid double input
+    // LCD.WriteLine("Touch screen to record value (1/12)");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    // // Write the value returned by the optosensor to the screen
+    // float leftOptosensorValue = left_opto.Value();
+    // LCD.Write("Left Optosensor Value:");
+    // LCD.WriteLine(leftOptosensorValue);
+    // Sleep(5.0); // Wait to avoid double input
+
+    // // Left Optosensor off straight line
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Place left optosensor off straight line");
+    // Sleep(0.25); // Wait to avoid double input
+    // LCD.WriteLine("Touch screen to record value (2/12)");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    // // Write the value returned by the optosensor to the screen
+    // leftOptosensorValue = left_opto.Value();
+    // // <ADD CODE HERE>
+    // LCD.Write(leftOptosensorValue);
+    // Sleep(5.0); // Wait to avoid double input
+
+    // // Repeat process for remaining optosensors, and repeat all three for the curved line values
+    // // <ADD CODE HERE>
+
+    // // Record values for optosensors on and off of the straight line
+    // // Right Optosensor on straight line
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Place right sensor on straight line");
+    // Sleep(5.0); // Wait to avoid double input
+    // LCD.WriteLine("Touch screen to record value (1/12)");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    // // Write the value returned by the optosensor to the screen
+    // float rightOptosensorValue = right_opto.Value();
+    // LCD.Write("Right Optosensor Value:");
+    // LCD.WriteLine(rightOptosensorValue);
+    // Sleep(5.0); // Wait to avoid double input
+
+    // // Left Optosensor off straight line
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Place right sensor off straight line");
+    // Sleep(5.0); // Wait to avoid double input
+    // LCD.WriteLine("Touch screen to record value (2/12)");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    // // Write the value returned by the optosensor to the screen
+    // rightOptosensorValue = right_opto.Value();
+    // // <ADD CODE HERE>
+    // LCD.WriteLine(rightOptosensorValue);
+    // Sleep(5.0); // Wait to avoid double input
+
+    // // Record values for optosensors on and off of the straight line
+    // // Center Optosensor on straight line
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Place middle ensor on straight line");
+    // Sleep(5.0); // Wait to avoid double input
+    // LCD.WriteLine("Touch screen to record value (1/12)");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    // // Write the value returned by the optosensor to the screen
+    // float centerOptosensorValue = center_opto.Value();
+    // LCD.Write("Center Optosensor Value:");
+    // LCD.WriteLine(centerOptosensorValue);
+    // Sleep(5.0); // Wait to avoid double input
+
+    // // Left Optosensor off straight line
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Place middle ensor off straight line");
+    // Sleep(1.0); // Wait to avoid double input
+    // LCD.WriteLine("Touch screen to record value (2/12)");
+    // while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    // while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    // // Write the value returned by the optosensor to the screen
+    //  centerOptosensorValue = center_opto.Value();
+    // // <ADD CODE HERE>
+    // LCD.WriteLine(centerOptosensorValue);
+    // Sleep(5.0); // Wait to avoid double input
+
+    // // Print end message to screen
+    // LCD.Clear(BLACK);
+    // LCD.WriteLine("Test Finished");
 }
-        
-    // while (!turned)
-    // {
-    //     right_motor.SetPercent(20);
-    //     left_motor.SetPercent(20);
-
-    //     while (one.Value() != 0)
-    //     {
-    //     }
-
-    //     right_motor.Stop();
-    //     left_motor.Stop();
-    //     Sleep(1.0);
-
-    //     if (turnCount == 0)
-    //     {
-    //         right_motor.SetPercent(40);
-    //         left_motor.SetPercent(5);
-    //         Sleep(2.0);
-    //     }
-    //     else if (turnCount == 1)
-    //     {
-    //         right_motor.SetPercent(40);
-    //         left_motor.SetPercent(5);
-    //         Sleep(2.0);
-    //     }
-    //     else if (turnCount == 2)
-    //     {
-    //         right_motor.SetPercent(40);
-    //         left_motor.SetPercent(5);
-    //         Sleep(2.0);
-    //     }
-    //     else
-    //     {
-    //         turned = true;
-    //     }
-
-    //     right_motor.Stop();
-    //     left_motor.Stop();
-    //     Sleep(2.0);
-
-    //     turnCount++;
-    // }
-
-    // right_motor.Stop();
-    // left_motor.Stop();
-
-
-
-
-
 
---
+++ src/old.cpp
@@ -0,0 +1,131 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     DigitalInputPin one(FEHIO::Pin11);
+//     DigitalInputPin two(FEHIO::Pin2);
+
+//     FEHMotor right_motor(FEHMotor::Motor3, 9.0);
+//     FEHMotor left_motor(FEHMotor::Motor2, 9.0);
+
+
+//     right_motor.SetPercent(20);
+//     left_motor.SetPercent(-20);
+    
+
+//     while (true) {
+//         while (one.Value() != 0) {};
+//         right_motor.Stop();
+//         left_motor.Stop();
+//         Sleep(1.0);
+//         right_motor.SetPercent(-20);
+//         left_motor.SetPercent(5);
+//         Sleep(3.0);
+//         right_motor.Stop();
+//         left_motor.Stop();
+//         right_motor.SetPercent(20);
+//         left_motor.SetPercent(-20);
+
+//         while (one.Value() != 0) {};
+//         right_motor.Stop();
+//         left_motor.Stop();
+//         Sleep(1.0);
+//         right_motor.SetPercent(5);
+//         left_motor.SetPercent(20);
+//         Sleep(3.0);
+//         right_motor.Stop();
+//         left_motor.Stop();
+//         right_motor.SetPercent(20);
+//         left_motor.SetPercent(-20);
+
+//         // while (one.Value() != 0) {};
+//         // right_motor.Stop();
+//         // left_motor.Stop();
+//         // Sleep(1.0);
+//         // right_motor.SetPercent(20);
+//         // left_motor.SetPercent(-5);
+//         // Sleep(4.0);
+//         // right_motor.SetPercent(20);
+//         // left_motor.SetPercent(20);
+    
+//     }
+// }
+        
+//     // while (!turned)
+//     // {
+//     //     right_motor.SetPercent(20);
+//     //     left_motor.SetPercent(20);
+
+//     //     while (one.Value() != 0)
+//     //     {
+//     //     }
+
+//     //     right_motor.Stop();
+//     //     left_motor.Stop();
+//     //     Sleep(1.0);
+
+//     //     if (turnCount == 0)
+//     //     {
+//     //         right_motor.SetPercent(40);
+//     //         left_motor.SetPercent(5);
+//     //         Sleep(2.0);
+//     //     }
+//     //     else if (turnCount == 1)
+//     //     {
+//     //         right_motor.SetPercent(40);
+//     //         left_motor.SetPercent(5);
+//     //         Sleep(2.0);
+//     //     }
+//     //     else if (turnCount == 2)
+//     //     {
+//     //         right_motor.SetPercent(40);
+//     //         left_motor.SetPercent(5);
+//     //         Sleep(2.0);
+//     //     }
+//     //     else
+//     //     {
+//     //         turned = true;
+//     //     }
+
+//     //     right_motor.Stop();
+//     //     left_motor.Stop();
+//     //     Sleep(2.0);
+
+//     //     turnCount++;
+//     // }
+
+//     // right_motor.Stop();
+//     // left_motor.Stop();
+
+
+
+
+
+
